e8a3f3d0d18e6497f5043c703e0c49cc33a6fee1
eljily <eljilysidibrahim@gmail.com>
2024-10-23 09:51:30 +0000

    project kickoff

---
+++ src/main/java/com/example/etud_app/EtudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.etud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/etud_app/EtudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
